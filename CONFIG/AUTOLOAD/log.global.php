e742251ce1abeef33ce62de62fa1dde61c68a86c
Jason Medland <jason@turntwotech.com>
2015-04-08 17:18:17 -0400

    basic updates

---
+++ CONFIG/AUTOLOAD/log.global.php
@@ -0,0 +1,41 @@
+<?php 
+
+return array(
+	"JCORE_LOG" => array(
+		"JCORE" => array(
+			"logFacility"=>"FILE",
+			"writePath"=>"/var/log/httpd/",
+			"logName"=>"JCORE_",
+			"dateFormat"=>"Y-m-d H:i:s",
+			"logSuffix"=>"log",
+			"stripWhitespace"=>TRUE,
+			"bufferWrite"=>FALSE,
+			#;blockSize=[4096]
+		),
+		"JCORE_DATA_LOG" => array(
+			"logFacility"=>"FILE",
+			"writePath"=>"/var/log/httpd/",
+			"logName"=>"JCORE_DATALOG_",
+			"dateFormat"=>"Y-m-d H:i:s",
+			"logSuffix"=>"log",
+			"stripWhitespace"=>TRUE,
+			"bufferWrite"=>FALSE,
+			#;blockSize=[4096]
+		),
+		"JCORE_CACHE_LOG" => array(
+			"logFacility"=>"FILE",
+			"writePath"=>"/var/log/httpd/",
+			"logName"=>"JCORE_CACHELOG_",
+			"dateFormat"=>"Y-m-d H:i:s",
+			"logSuffix"=>"log",
+			"stripWhitespace"=>TRUE,
+			"bufferWrite"=>FALSE,
+			#;blockSize=[4096]
+		),
+	)
+
+);
+
+
+
+?>
--- CONFIG/AUTOLOAD/log.php
+++ CONFIG/AUTOLOAD/log.php
@@ -2,36 +2,7 @@
 
 return array(
 	"JCORE_LOG" => array(
-		"JCORE" => array(
-			"logFacility"=>"FILE",
-			"writePath"=>"/var/log/",
-			"logName"=>"JCORE_",
-			"dateFormat"=>"Y-m-d H:i:s",
-			"logSuffix"=>"log",
-			"stripWhitespace"=>TRUE,
-			"bufferWrite"=>FALSE,
-			#;blockSize=[4096]
-		),
-		"JCORE_DATA_LOG" => array(
-			"logFacility"=>"FILE",
-			"writePath"=>"/var/log/",
-			"logName"=>"JCORE_DATALOG_",
-			"dateFormat"=>"Y-m-d H:i:s",
-			"logSuffix"=>"log",
-			"stripWhitespace"=>TRUE,
-			"bufferWrite"=>FALSE,
-			#;blockSize=[4096]
-		),
-		"JCORE_CACHE_LOG" => array(
-			"logFacility"=>"FILE",
-			"writePath"=>"/var/log/",
-			"logName"=>"JCORE_CACHELOG_",
-			"dateFormat"=>"Y-m-d H:i:s",
-			"logSuffix"=>"log",
-			"stripWhitespace"=>TRUE,
-			"bufferWrite"=>FALSE,
-			#;blockSize=[4096]
-		),
+
 		/*
 		* settings per log type
 		*-------------------------------
