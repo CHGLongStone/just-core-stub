19ef98d58cc75b875407c4adde10390a64444e4a
Jason Medland <jason.medland@gmail.com>
2015-06-26 02:05:06 -0400

    stuff...ad things...

--- CONFIG/AUTOLOAD/auth.global.php
+++ CONFIG/AUTOLOAD/auth.global.php
@@ -1,8 +1,12 @@
 <?php 
 /**
 * examples using local host and standard internal network IP's 
+* integrated this project as a user auth baseline
+* https://github.com/hautelook/phpass
+*
 * TODO:
 * write a regex to compare by subnet mask rather than specific IP's
+*
 */
 
 return array(
@@ -19,6 +23,23 @@ return array(
 					'192.168.0.1',
 					'10.0.0.1',
 				),
+			),
+		),
+		/*
+		* see constructor here:
+		* https://github.com/hautelook/phpass/blob/master/src/Hautelook/Phpass/PasswordHash.php
+		* http://www.openwall.com/phpass/
+		*/
+		'PHPASS' => array(
+			'FORM1' => array( //these are profile sections 
+				'iteration_count_log2' => 8, 
+				'portable_hashes' => false,
+				'DSN' =>	'blackwatch',
+				'TABLE_NAME' =>	'client',
+				'USR_COLUMN' =>	'user_name',
+				'PWD_COLUMN' =>	'password',
+				'FORM_USR' =>	'usr',
+				'FORM_PWD' =>	'pwd',
 			),
 		),
     ),
